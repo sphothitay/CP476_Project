2caefc73956353329a015212e58a167ac86b69e9
Jacob Heard <heard.jacob.7@gmail.com>
2020-03-03 22:58:36 -0500

    Added some utility functions for querying mysql database

---
+++ sql_queries.py
@@ -0,0 +1,54 @@
+import mysql.connector
+
+def GetDB():
+	return mysql.connector.connect(
+		host="localhost",
+		user="root",
+		passwd="",
+		database="")
+
+def GetArgument( db, argumentID ):
+	queryString = '''SELECT * FROM Arguments AS A
+INNER JOIN Topics as T
+ON A.TopicID=T.TopicID
+WHERE ArgumentID=%s'''
+	return runQuery( db, queryString, (argumentID, ) )
+
+def GetComment( db, commentID ):
+	queryString = 'SELECT * FROM Comments WHERE CommentID=%s'
+	return runQuery( db, queryString, (commentID, ) )
+
+def GetUser( db, userID ):
+	queryString = 'SELECT * FROM Users WHERE UserID=%s'
+	return runQuery( db, queryString, (userID, ) )
+
+def GetUserByUsername( db, username ):
+	queryString = 'SELECT * FROM Users WHERE Username=%s'
+	return runQuery( db, queryString, (username, ) )
+
+def GetArgumentVoteCount( db, argumentID ):
+	queryString = '''SELECT SUM( CASE WHEN IsUpvote THEN 1 ELSE -1 ) as NumVotes
+FROM Votes WHERE ArgumentID=%s'''
+	return runQuery( db, queryString, (argumentID, ) )
+	
+def GetCommentVoteCount( db, commentID ):
+	queryString = '''SELECT SUM( CASE WHEN IsUpvote THEN 1 ELSE -1 ) as NumVotes
+FROM CommentVotes WHERE CommentID=%s'''
+	return runQuery( db, queryString, (commentID, ) )
+
+def GetUserCommentVoteCount( db, userID ):
+	queryString = '''SELECT SUM( CASE WHEN IsUpvote THEN 1 ELSE -1 ) as NumVotes
+FROM CommentVotes WHERE UserID=%s'''
+	return runQuery( db, queryString, (userID, ) )
+
+def GetUserArgumentVoteCount( db, userID ):
+	queryString = '''SELECT SUM( CASE WHEN IsUpvote THEN 1 ELSE -1 ) as NumVotes
+FROM Votes WHERE UserID=%s'''
+	return runQuery( db, queryString, (userID, ) )
+
+def runQuery( db, queryString, params ):
+	cursor = db.cursor()
+	cursor.execute( queryString, params )
+	result = cursor.fetchall()
+	cursor.close()
+	return result
