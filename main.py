3f4529ceed97ab4608b1257d280eda4d8f344e76
Jacob Heard <heard.jacob.7@gmail.com>
2020-03-04 00:25:00 -0500

    Added render_template to serve HTML files so far

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
+@app.route('/home')
 def index():
-    return "TODO: main page"
+    return render_template( 'index.html' )
 
 @app.route('/login', methods=['POST'])
 def login():
-	return "TODO: login page"
+	return "TODO: login user"
 
 @app.route('/register', methods=['POST'])
 def register():
-	return "TODO: Register page"
+	return "TODO: Register user"
 
+@app.route('/debate')
+def debate():
+	return render_template( 'debate.html' )
+
+@app.route('/topics')
+def topics():
+	return render_template( 'topics.html' )
+
+@app.route('/createTopic')
+def createTopic():
+	return render_template( 'createTopic.html' )
