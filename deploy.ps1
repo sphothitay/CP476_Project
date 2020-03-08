d9f7e3064f1514b62cc723ccbffdd768ab9c4a3a
Jacob Heard <heard.jacob.7@gmail.com>
2020-03-08 00:24:29 -0500

    cp467 -> cp476

--- deploy.ps1
+++ deploy.ps1
@@ -2,14 +2,14 @@
 $mysqlpass = -join ((65..90) + (97..122) | Get-Random -Count 32 | % {[char]$_})
 
 Write-Host "Removing old installation (if one exists)"
-docker container rm -f cp467-app
-docker image rm -f cp467-app
+docker container rm -f cp476-app
+docker image rm -f cp476-app
 
 Write-Host "Building the docker image"
-docker build -t cp467-app .
+docker build -t cp476-app .
 
-Write-Host "Deploying cp467-app website"
-docker run --detach --name cp467-app -e MYSQL_PASSWORD=$mysqlpass -p 127.0.0.1:80:5000/tcp cp467-app
+Write-Host "Deploying cp476-app website"
+docker run --detach --name cp476-app -e MYSQL_PASSWORD=$mysqlpass -p 127.0.0.1:80:5000/tcp cp476-app
 
 Write-Host 'Finished. Press any key to continue...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
