eb6f995a1497b5975b7b7019eb7dc7908546e59d
Jacob Heard <heard.jacob.7@gmail.com>
2020-04-28 12:53:09 -0400

    Better test data

--- sql/load_test_data.sql
+++ sql/load_test_data.sql
@@ -1,38 +1,45 @@
 USE DebateDB;
 
 INSERT INTO Users (Username, Password)
-VALUES ('User1', 'This-Will-Be-A-Hash');
+VALUES ('user1', '$2b$12$Oz124XzEkQnmJwtZtYtW2.eIJS4JyedMahttZ3BvpOzBtWE5JGZaK');
 INSERT INTO Users (Username, Password)
-VALUES ('User2', 'This-Will-Be-A-Hash');
+VALUES ('user2', '$2b$12$Oz124XzEkQnmJwtZtYtW2.eIJS4JyedMahttZ3BvpOzBtWE5JGZaK');
 INSERT INTO Users (Username, Password)
-VALUES ('User3', 'This-Will-Be-A-Hash');
+VALUES ('user3', '$2b$12$Oz124XzEkQnmJwtZtYtW2.eIJS4JyedMahttZ3BvpOzBtWE5JGZaK');
 INSERT INTO Users (Username, Password)
-VALUES ('User4', 'This-Will-Be-A-Hash');
+VALUES ('user4', '$2b$12$Oz124XzEkQnmJwtZtYtW2.eIJS4JyedMahttZ3BvpOzBtWE5JGZaK');
 INSERT INTO Users (Username, Password)
-VALUES ('User5', 'This-Will-Be-A-Hash');
+VALUES ('user5', '$2b$12$Oz124XzEkQnmJwtZtYtW2.eIJS4JyedMahttZ3BvpOzBtWE5JGZaK');
 
 INSERT INTO Topics (TopicName, TopicDescription)
 VALUES ('Wildlife', 'Talk about wildlife here!');
 INSERT INTO Topics (TopicName, TopicDescription)
 VALUES ('Politics', 'A topic for debating all kinds of politics');
 INSERT INTO Topics (TopicName, TopicDescription)
-VALUES ('Fake Topic', 'Fake Description');
+VALUES ('Veganism', 'yum yum veggies');
 INSERT INTO Topics (TopicName, TopicDescription)
-VALUES ('Blank Topic', '');
+VALUES ('Technology', '');
+INSERT INTO Topics (TopicName, TopicDescription)
+VALUES ('Law', '');
+INSERT INTO Topics (TopicName, TopicDescription)
+VALUES ('Television', '');
 
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, User1ID, User2ID, TopicID)
-VALUES ('Argument 1', 'This is the argument body; need to figure out how to format this', 1, 2, 1);
+VALUES ('We should embrace communism', '', 1, 2, 1);
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, User1ID, User2ID, TopicID)
-VALUES ('Argument 2', '', 2, 3, 2);
+VALUES ('Hot dogs are not sandwiches', 'They are closed on one side its not a sandwich just a whiter taco', 2, 3, 2);
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, User1ID, User2ID, TopicID)
-VALUES ('Another Argument', 'This is the first argument with the name "Another Argument"', 3, 1, 4);
+VALUES ('Ketchup is a valid sundae topping', '', 3, 1, 4);
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, User1ID, User2ID, TopicID)
 VALUES ('Another Argument', 'This just shows that multiple arguments can have the same name', 3, 4, 2);
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, User1ID, TopicID)
 VALUES ('Opinion', 'This just shows that multiple arguments can have the same name', 1, 2);
 
 INSERT INTO Messages (MessageContent, ArgumentID, UserID)
-VALUES ('opinion', 1, 1);
+VALUES ('No.', 3, 1);
+INSERT INTO Messages (MessageContent, ArgumentID, UserID)
+VALUES ('Subs are the same, does that make them not sandwiches?', 2, 3);
+
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, UserID, TopicID)
 VALUES ('FACTS', 1, 2);
 INSERT INTO Arguments (ArgumentTitle, ArgumentContent, UserID, TopicID)
